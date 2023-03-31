bab2023f85f7d3ad83f486e78ad62ba0bcdff427
Paul Frank <pail@gmx.net>
2023-03-31 16:40:48 +0000

    Improve climate entity

--- custom_components/stiebel_eltron_isg/climate.py
+++ custom_components/stiebel_eltron_isg/climate.py
@@ -15,8 +15,10 @@ from homeassistant.const import UnitOfTemperature
 from .const import (
     DOMAIN,
     ACTUAL_HUMIDITY,
+    ACTUAL_TEMPERATURE,
     ACTUAL_TEMPERATURE_FEK,
     COMFORT_TEMPERATURE_TARGET_HK1,
+    OPERATION_MODE
 )
 from .entity import StiebelEltronISGEntity
 
@@ -24,12 +26,35 @@ _LOGGER = logging.getLogger(__name__)
 
 CLIMATE_HK_1 = "climate_hk_1"
 
-CLIMATE_HK_1_MAPPING = {
-    "humidity": ACTUAL_HUMIDITY,
-    "actual_temperature": ACTUAL_TEMPERATURE_FEK,
-    "target_temperature": COMFORT_TEMPERATURE_TARGET_HK1,
+WPM_TO_HA_HVAC = {
+    1: HVACMode.AUTO,
+    2: HVACMode.AUTO,
+    3: HVACMode.AUTO,
+    4: HVACMode.AUTO,
+    5: HVACMode.OFF,
+    0: HVACMode.AUTO,
 }
 
+HA_TO_WPM_HVAC = {
+    HVACMode.AUTO: 2,
+    HVACMode.OFF: 5,
+}
+
+LWZ_TO_HA_HVAC = {
+    11: HVACMode.AUTO,
+    14: HVACMode.HEAT,
+    1: HVACMode.AUTO,
+    3: HVACMode.AUTO,
+    4: HVACMode.AUTO,
+    5: HVACMode.OFF,
+    0: HVACMode.AUTO,
+}
+
+HA_TO_LWZ_HVAC = {
+    HVACMode.AUTO: 11,
+    HVACMode.OFF: 5,
+    HVACMode.HEAT: 14,
+}
 
 CLIMATE_TYPES = [
     ClimateEntityDescription(CLIMATE_HK_1, has_entity_name=True, name="Heat Circuit 1")
@@ -42,30 +67,28 @@ async def async_setup_entry(hass, entry, async_add_devices):
 
     entities = []
     for description in CLIMATE_TYPES:
-        climate_entity = StiebelEltronISGClimateEntity(
-            coordinator, entry, description, CLIMATE_HK_1_MAPPING
-        )
+        if coordinator.is_wpm:
+            climate_entity = StiebelEltronWPMClimateEntity(
+                coordinator, entry, description
+            ) if coordinator.is_wpm else StiebelEltronLWZClimateEntity(
+                coordinator, entry, description
+            )
         entities.append(climate_entity)
     async_add_devices(entities)
 
 
 class StiebelEltronISGClimateEntity(StiebelEltronISGEntity, ClimateEntity):
-    """stiebel_eltron_isg select class."""
+    """stiebel_eltron_isg climate class."""
 
-    def __init__(self, coordinator, config_entry, description, mapping):
-        """Initialize the select entity."""
+    def __init__(self, coordinator, config_entry, description):
+        """Initialize the climate entity."""
         self.entity_description = description
-        self.mapping = mapping
         self._attr_temperature_unit = UnitOfTemperature.CELSIUS
 
         self._attr_supported_features = ClimateEntityFeature.TARGET_TEMPERATURE
         self._attr_target_temperature_low = 5
         self._attr_target_temperature_high = 30
         self._attr_target_temperature_step = 0.1
-
-        # todo: implement this
-        self._attr_hvac_modes = [HVACMode.HEAT, HVACMode.OFF]
-        self._attr_hvac_mode = HVACMode.HEAT
 
         super().__init__(coordinator, config_entry)
 
@@ -77,19 +100,58 @@ class StiebelEltronISGClimateEntity(StiebelEltronISGEntity, ClimateEntity):
     @property
     def current_humidity(self) -> int | None:
         """Return the current humidity."""
-        return self.coordinator.data.get(self.mapping["humidity"])
+        return self.coordinator.data.get(ACTUAL_HUMIDITY)
 
     @property
     def current_temperature(self) -> float | None:
         """Return the current temperature."""
-        return self.coordinator.data.get(self.mapping["actual_temperature"])
+        temperature = self.coordinator.data.get(ACTUAL_TEMPERATURE)
+        return temperature if temperature is not None else self.coordinator.data.get(ACTUAL_TEMPERATURE_FEK)
 
     @property
     def target_temperature(self) -> float | None:
         """Return the temperature we try to reach."""
-        return self.coordinator.data.get(self.mapping["target_temperature"])
+        return self.coordinator.data.get(COMFORT_TEMPERATURE_TARGET_HK1)
 
     def set_temperature(self, **kwargs) -> None:
         """Set new target temperature."""
         value = kwargs["temperature"]
-        self.coordinator.set_data(self.mapping["target_temperature"], value)
+        self.coordinator.set_data(COMFORT_TEMPERATURE_TARGET_HK1, value)
+
+
+class StiebelEltronWPMClimateEntity(StiebelEltronISGClimateEntity):
+
+    def __init__(self, coordinator, config_entry, description):
+        """Initialize the climate entity."""
+        self._attr_hvac_modes = [HVACMode.AUTO, HVACMode.OFF]
+        super().__init__(coordinator, config_entry, description)
+
+
+    @property
+    def hvac_mode(self) -> HVACMode | None:
+        """Return current operation ie. heat, cool, idle."""
+        return WPM_TO_HA_HVAC.get(self.coordinator.data.get(OPERATION_MODE))
+
+    def set_hvac_mode(self, hvac_mode: HVACMode) -> None:
+        """Set new operation mode."""
+        new_mode = HA_TO_WPM_HVAC.get(hvac_mode)
+        self.coordinator.set_data(OPERATION_MODE, new_mode)
+
+
+class StiebelEltronLWZClimateEntity(StiebelEltronISGClimateEntity):
+
+    def __init__(self, coordinator, config_entry, description):
+        """Initialize the climate entity."""
+        self._attr_hvac_modes = [HVACMode.AUTO, HVACMode.OFF, HVACMode.MANUAL]
+        super().__init__(coordinator, config_entry, description)
+
+
+    @property
+    def hvac_mode(self) -> HVACMode | None:
+        """Return current operation ie. heat, cool, idle."""
+        return LWZ_TO_HA_HVAC.get(self.coordinator.data.get(OPERATION_MODE))
+
+    def set_hvac_mode(self, hvac_mode: HVACMode) -> None:
+        """Set new operation mode."""
+        new_mode = HA_TO_LWZ_HVAC.get(hvac_mode)
+        self.coordinator.set_data(OPERATION_MODE, new_mode)
