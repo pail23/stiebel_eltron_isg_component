4d8ef034c80f98e7c0d97add3ac6ea0e994cc417
Paul Frank <pail@gmx.net>
2023-05-15 22:38:10 +0200

    Fixing lwz Binary sensors

--- custom_components/stiebel_eltron_isg/__init__.py
+++ custom_components/stiebel_eltron_isg/__init__.py
@@ -532,11 +532,11 @@ class StiebelEltronModbusLWZDataCoordinator(StiebelEltronModbusDataCoordinator):
                 inverter_data.registers, byteorder=Endian.Big
             )
             state = decoder.decode_16bit_uint()
-            result[COMPRESSOR_ON] = (state & (1 << 2)) != 0
-            result[IS_HEATING] = (state & (1 << 3)) != 0
-            result[IS_COOLING] = (state & (1 << 4)) != 0
-            result[IS_HEATING_WATER] = (state & (1 << 5)) != 0
-            result[PUMP_ON_HK1] = (state & (1 << 11)) != 0
+            result[COMPRESSOR_ON] = (state & (1 << 1)) != 0
+            result[IS_HEATING] = (state & (1 << 2)) != 0
+            result[IS_COOLING] = (state & (1 << 3)) != 0
+            result[IS_HEATING_WATER] = (state & (1 << 4)) != 0
+            result[PUMP_ON_HK1] = (state & (1 << 10)) != 0
             #  result[IS_SUMMER_MODE] = (state & (1 << 7)) != 0
 
             result[ERROR_STATUS] = decoder.decode_16bit_uint()
