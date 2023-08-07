468aca202dbfc7c1464067009f04987b273af76d
Paul Frank <pail@gmx.net>
2023-08-07 16:20:15 +0000

    spelling

--- custom_components/stiebel_eltron_isg/const.py
+++ custom_components/stiebel_eltron_isg/const.py
@@ -89,7 +89,7 @@ EVAPORATOR_DEFROST = "evaporator_defrost"
 FILTER_EXTRACT_AIR = "filter_extract_air"
 FILTER_VENTILATION_AIR = "filter_ventilation_air"
 HEAT_UP_PROGRAM = "heat_up_program"
-NHZ_STAGES_RUNNING = "nht_stages_running"
+NHZ_STAGES_RUNNING = "nhz_stages_running"
 
 COMFORT_TEMPERATURE_TARGET_HK1 = "comfort_temperature_target_hk1"
 ECO_TEMPERATURE_TARGET_HK1 = "eco_temperature_target_hk1"
