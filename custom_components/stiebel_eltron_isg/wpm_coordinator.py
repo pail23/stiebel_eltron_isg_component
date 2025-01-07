da3e6e503f5e985914c945a043745fc9c16d7f1e
dickesW <42518853+dickesW@users.noreply.github.com>
2025-01-07 17:35:38 +0100

    Update wpm_coordinator.py

--- custom_components/stiebel_eltron_isg/wpm_coordinator.py
+++ custom_components/stiebel_eltron_isg/wpm_coordinator.py
@@ -196,7 +196,7 @@ class StiebelEltronModbusWPMDataCoordinator(StiebelEltronModbusDataCoordinator):
             result[IS_COOLING] = (state & (1 << 8)) != 0
             result[EVAPORATOR_DEFROST] = (state & (1 << 9)) != 0
 
-            decoder.skip_bytes(4)
+            result[EVU] =  = (state & (1 << 0)) != 0
             result[ERROR_STATUS] = decoder.decode_16bit_uint()
             decoder.skip_bytes(4)
             # 2507
