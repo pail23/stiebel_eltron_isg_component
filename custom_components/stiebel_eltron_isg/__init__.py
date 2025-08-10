70045809fabde9a11076fca97b5180a5347d23cb
Paul Frank <pail@gmx.net>
2025-08-10 20:39:41 +0000

    apply changes from the template

--- custom_components/stiebel_eltron_isg/__init__.py
+++ custom_components/stiebel_eltron_isg/__init__.py
@@ -121,5 +121,4 @@ async def async_reload_entry(
     entry: StiebelEltronIsgIntegrationConfigEntry,
 ) -> None:
     """Reload config entry."""
-    await async_unload_entry(hass, entry)
-    await async_setup_entry(hass, entry)
+    await hass.config_entries.async_reload(entry.entry_id)
